6ec770605a4158bfbbd94755cc64dcb3bf41e045
Genilson Alves <171091849+genilssom@users.noreply.github.com>
2026-05-12 12:44:19 -0300

    Update get_code_packages.sh

--- codes/get_code_packages.sh
+++ codes/get_code_packages.sh
@@ -34,7 +34,7 @@ rm -fr kompost_code/.git
 
 # download MUSIC
 rm -fr MUSIC_code
-git clone https://github.com/MUSIC-fluid/MUSIC -b public_stable MUSIC_code
+git clone --depth=3 https://github.com/luizafperin/MUSIC -b main MUSIC_code
 #(cd MUSIC_code; git checkout e898cc826efc0303f2dd688602e6c604c9ef0f91)
 rm -fr MUSIC_code/.git
 
@@ -105,5 +105,5 @@ rm -fr hadronic_afterburner_toolkit_code/.git
 (cd 3dMCGlauber_code/tables; bash download_nucleusTables.sh;)
 # download nucleus configurations for IP-Glasma
 (cd ipglasma_code/nucleusConfigurations; bash download_nucleusTables.sh;)
-# download essential EOS files for hydro simulations
-(cd MUSIC_code/EOS; bash download_hotQCD.sh; bash download_hotQCD.sh SMASH_binary; bash download_Neos2D.sh bqs;)
+# download essential EOS files for hydro simulations for public_stable MUSIC
+(cd MUSIC_code/EOS/; bash download_hotQCD.sh; bash download_hotQCD.sh SMASH_binary; bash download_Neos2D.sh bqs; bash download_EOS-gp.sh)
